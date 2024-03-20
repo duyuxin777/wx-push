490166af6aea8011008941242c396acb2b5d51b5
duyuxin777 <164121198+duyuxin777@users.noreply.github.com>
2024-03-20 11:11:42 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wxff441173ca9a0e39",
+  appsecret: "bc443a695a8be86ced517c64eea27433",
+  template_id: "2mOdxU5XREulH2BIfFjMLP3i46R-_RR3F9jRsTrQGWo",
+  touser: "oRwqe6KZmVmIxwwkugV5Ptwkiz0k",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
+  city: "北京",
   first_date: "2022-11-10",
 };
